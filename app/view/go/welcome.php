ef1e1449a6054106db5406eb4634c359462ce4e8
JPfleger <jpfleger83@gmail.com>
2016-06-13 22:38:01 -0400

    native bootstrap

--- app/view/go/welcome.php
+++ app/view/go/welcome.php
@@ -1,10 +1,11 @@
 <html>
     <head>
+        <?= $go->load->css('bootstrap'); ?>
         <?= $go->load->css('style'); ?>
     </head>
     <body>
         <div class="container">
-            <h2>Welcome to Go</h2>
+            <h2>Welcome to GO</h2>
             <strong>
                 You will need to set the permissions of the config file (best to chown to your ownership) and set the config file.<br/>
             Then fill out the config array to match your dev environment.
